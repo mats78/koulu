c03eab08e573a04395d093d787ae15aa1b2dab98
Matias <matias.kallio@gmail.com>
2021-09-10 16:03:11 +0300

    Muunnin toimii

--- jscript.js
+++ jscript.js
@@ -2,11 +2,11 @@ console.log("tiedosto2")
 
 const fahrenheitSyote = document.querySelector('input')
 const fahrenheitTuloste = document.querySelector('#fahrenheit')
-const celciusTuloste = document.querySelector('#celcius')
+const celciusTuloste = document.querySelector('#celsius')
 const button = document.querySelector('button')
 
 function fahrenheitCelciukseksi(fahrenheit){
-    let celcius = (fahrenheit-32) / 1.8;
+    let celcius = ((fahrenheit-32) / 1.8);
     console.log(celcius);
     return celcius;
 }
